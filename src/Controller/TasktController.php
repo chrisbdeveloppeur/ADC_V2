e619c09f19701fef3888e4a3d83206f22d64fb14
chrisbdeveloppeur <kenshin91cb@gmail.com>
2021-01-01 18:36:48 +0100

    MAJ DESKTOP update composer & add previous page btn JS

--- src/Controller/TasktController.php
+++ src/Controller/TasktController.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/", name="taskt_")
+ * @Route("taskt/", name="taskt_")
  */
 class TasktController extends AbstractController
 {
