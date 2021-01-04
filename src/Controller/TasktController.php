db57d71b50626fce85ee6ec3921ca913e005d506
K4gur0 <kenshin91cb@gmail.com>
2021-01-04 10:41:54 +0100

    MAJ LAPTOP maj list hostname to --400000 assets v1.9.2

--- src/Controller/TasktController.php
+++ src/Controller/TasktController.php
@@ -180,9 +180,9 @@ class TasktController extends AbstractController
 ////////////////////////////////////////////////////////////////////////////////////////
 ///
         //////////////////         EN LOCAL HOST          //////////////////
-//        $csv = '..\assets\csv\postes.csv';
+        $csv = '..\assets\csv\postes.csv';
         //////////////////         EN SERVEUR PROD        //////////////////
-        $csv = '/home/scctcrvc/public_html/scc-tool/assets/csv/postes.csv';
+//        $csv = '/home/scctcrvc/public_html/scc-tool/assets/csv/postes.csv';
         $arrayCsv = file($csv);
 //        $file = fopen($csv, 'r');
 
