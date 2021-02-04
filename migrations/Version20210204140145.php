f6378f37ddbccd3c6158eaa748b129b67478f44b
chrisbdevelopper <kenshin91cb@gmail.com>
2021-02-04 15:02:22 +0100

    MAJ DESKTOP modification de l'entité Rdv

--- migrations/Version20210204140145.php
+++ migrations/Version20210204140145.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210202132223 extends AbstractMigration
+final class Version20210204140145 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,12 @@ final class Version20210202132223 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE survey ADD chemin LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\'');
+        $this->addSql('ALTER TABLE rdv DROP rdv_total, DROP rdv_ko_scc, DROP rdv_ko_safran');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE survey DROP chemin');
+        $this->addSql('ALTER TABLE rdv ADD rdv_total INT DEFAULT NULL, ADD rdv_ko_scc INT DEFAULT NULL, ADD rdv_ko_safran INT DEFAULT NULL');
     }
 }
