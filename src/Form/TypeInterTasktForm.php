c9e43911dd2fa210d220e6a28af284315d5ecb80
chrisbdevelopper <kenshin91cb@gmail.com>
2021-02-17 19:31:59 +0100

    MAJ DESKTOP add dynamic formType with survey Entity Data (Use __construct)

--- src/Form/TypeInterTasktForm.php
+++ src/Form/TypeInterTasktForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use App\Controller\SurveySessionController;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -9,24 +10,51 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class TypeInterTasktForm extends AbstractType
 {
+    private $survey;
+
+    public function __construct(SurveySessionController $surveySessionController)
+    {
+        $this->survey =  $surveySessionController->checkSurveySession();
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('type', ChoiceType::class,
-                array('label' => false,
-                    'required' => true,
-                    'choices' => [
-                        'Fourniture, reprise et déménagement de postes de travail' => '1',
-                        'Fourniture, reprise et déménagement de matériels hors postes de travail' => '2',
-                        'Installation d’applications sans fourniture de matériel.' => '3',
-                        'Autres actions matériel' => '4',
-                        'Autres actions logiciel et accès' => '5',
-                        'Support téléphonie' => '6',
-                        'Actions CMDB' => '7',
-                    ]
+//        dd($this->survey->getService());
+        $service = $this->survey->getService();
+        if ($service == 'HD'){
+            $builder
+                ->add('type', ChoiceType::class,
+                    array('label' => false,
+                        'required' => true,
+                        'choices' => [
+                            'Installation d’applications sans fourniture de matériel' => '3',
+                            'Autres actions matériel' => '4',
+                            'Autres actions logiciel et accès' => '5',
+                            'Support téléphonie' => '6',
+                            'Actions CMDB' => '7',
+                        ]
+                    )
                 )
-            )
-        ;
+            ;
+        }else{
+            $builder
+                ->add('type', ChoiceType::class,
+                    array('label' => false,
+                        'required' => true,
+                        'choices' => [
+                            'Fourniture, reprise et déménagement de postes de travail' => '1',
+                            'Fourniture, reprise et déménagement de matériels hors postes de travail' => '2',
+                            'Installation d’applications sans fourniture de matériel' => '3',
+                            'Autres actions matériel' => '4',
+                            'Autres actions logiciel et accès' => '5',
+                            'Support téléphonie' => '6',
+                            'Actions CMDB' => '7',
+                        ]
+                    )
+                )
+            ;
+        }
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
