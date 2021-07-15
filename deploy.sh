d9dc116cb7fa416123ecb96e4aae0fca68b626ea
chrisbdevelopper <kenshin91cb@gmail.com>
2021-07-15 17:32:10 +0200

    DESKTOP modification du script en final string (afin d'éviter aux technicien de revenir en arrière en à la fin du formulaire

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -avz ./ -e "ssh -p 5022"  146.88.236.123:~/public_html/scc-tool --include=public/build --include=public/bundles --include=public/.htaccess --include=vendor --exclude-from=.gitignore --exclude=".*" --exclude="public/images"
+rsync -avz ./ -e "ssh -p 5022"  scctcrvc@world-328.fr.planethoster.net:~/public_html/scctool --include=public/build --include=public/bundles --include=public/.htaccess --include=vendor --exclude-from=.gitignore --exclude=".*" --exclude="public/images"
